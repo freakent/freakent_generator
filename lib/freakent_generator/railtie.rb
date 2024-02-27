06b5cb1d0fa609770884e99155cd5b55ad1b2b77
= <=>
2024-02-27 09:34:59 +0000

    add hook to ScaffoldController

--- lib/freakent_generator/railtie.rb
+++ lib/freakent_generator/railtie.rb
@@ -2,7 +2,9 @@ module FreakentGenerator
   class Railtie < ::Rails::Railtie
     config.app_generators do |g|
       g.templates.unshift File::expand_path('../../templates', __FILE__)
-      print g.templates
+      # print g.templates
     end
+
+    Rails::Generators::ScaffoldGenerator.hook_for :policies, as: :pundit ,type: :boolean
   end
 end
