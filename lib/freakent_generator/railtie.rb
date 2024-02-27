61ea8dfdc8e02c33111ee3dd51d3be65c5de7e7a
= <=>
2024-02-27 00:56:43 +0000

    tweaked railtie

--- lib/freakent_generator/railtie.rb
+++ lib/freakent_generator/railtie.rb
@@ -2,6 +2,7 @@ module FreakentGenerator
   class Railtie < ::Rails::Railtie
     config.app_generators do |g|
       g.templates.unshift File::expand_path('../templates', __FILE__)
+      print g.templates
     end
   end
 end
