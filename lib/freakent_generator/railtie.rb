2f9ffa37aca876d6add39e3d7a9b462dd65301fb
= <=>
2024-02-27 01:05:25 +0000

    changed path to templates

--- lib/freakent_generator/railtie.rb
+++ lib/freakent_generator/railtie.rb
@@ -1,7 +1,7 @@
 module FreakentGenerator
   class Railtie < ::Rails::Railtie
     config.app_generators do |g|
-      g.templates.unshift File::expand_path('../templates', __FILE__)
+      g.templates.unshift File::expand_path('../../templates', __FILE__)
       print g.templates
     end
   end
